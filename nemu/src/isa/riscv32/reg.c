0a193d762f85943ca9d9cc43ae605778cd28db57
krkawzq <2868116803@qq.com>
2025-01-28 23:12:49 +0800

    regs, info

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -27,7 +27,7 @@ void isa_reg_display() {
   printf("Register states:\n");
   for (int i = 0; i < 32; i += 4) {
     printf(
-      "%4s%x %4s%x %4s%x %4s%x\n",
+      "%-4s: 0x%08x %-4s: 0x%x %-4s: 0x%x %-4s: 0x%x\n",
       reg_name(i), gpr(i), reg_name(i + 1), gpr(i + 1),
       reg_name(i + 2), gpr(i + 2), reg_name(i + 3), gpr(i + 3)
     );
