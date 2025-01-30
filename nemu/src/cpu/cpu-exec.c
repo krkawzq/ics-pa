2275f52583a036632722b173275c4680f2b76b05
krkawzq <2868116803@qq.com>
2025-01-30 20:56:00 +0800

    增加了watch

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -31,7 +31,7 @@ static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
 
 void device_update();
-void check_watchpoint();
+bool check_watchpoint();
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
