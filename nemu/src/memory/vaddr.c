57dfb7073d24a0c412c25268484d7262e188851e
tracer-ics2024 <tracer@njuics.org>
2025-02-02 11:59:49 +0800

    >  compile NEMU 231220000 张三 Linux KraPc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:59:49 up  8:34,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -21,9 +21,9 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr + RESET_VECTOR, len);
+  return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  paddr_write(addr + RESET_VECTOR, len, data);
+  paddr_write(addr, len, data);
 }
