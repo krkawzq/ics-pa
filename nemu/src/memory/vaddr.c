5a8721b59abff5b160c19feeabd80b58f2636de5
tracer-ics2024 <tracer@njuics.org>
2025-02-03 16:09:38 +0800

    >  compile NEMU 231220000 张三 Linux KraPc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:09:38 up  1:19,  1 user,  load average: 0.05, 0.04, 0.00

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -16,14 +16,67 @@
 #include <isa.h>
 #include <memory/paddr.h>
 
+#ifdef CONFIG_MTRACE
+#define MTRACE_SIZE 32
+char mtrace_ringbuf[MTRACE_SIZE][128];
+int mtrace_idx = 0;
+size_t mtrace_count = 0;  // 添加计数器
+
+void mtrace_format(char *buf, vaddr_t addr, int len, word_t data, bool is_read) {
+  if (is_read) {
+    switch (len) {
+      case 1: sprintf(buf, "0x%08x: READ %d  %02x\n", addr, len, data); break;
+      case 2: sprintf(buf, "0x%08x: READ %d  %04x\n", addr, len, data); break;
+      case 4: sprintf(buf, "0x%08x: READ %d  %08x\n", addr, len, data); break;
+    }
+  } else {
+    switch (len) {
+      case 1: sprintf(buf, "0x%08x: WRITE %d  %02x\n", addr, len, data); break;
+      case 2: sprintf(buf, "0x%08x: WRITE %d  %04x\n", addr, len, data); break;
+      case 4: sprintf(buf, "0x%08x: WRITE %d  %08x\n", addr, len, data); break;
+    }
+  }
+}
+
+void mtrace_print() {
+  if (mtrace_count == 0) {
+    printf("No memory trace\n");
+    return;
+  }
+  if (mtrace_count < MTRACE_SIZE) {
+    for (int i = 0; i < mtrace_idx; i++) {
+      printf("%s", mtrace_ringbuf[i]);
+    }
+  } else {
+    for (int i = (mtrace_idx + 1) % MTRACE_SIZE; i < MTRACE_SIZE; i++) {
+      printf("%s", mtrace_ringbuf[i]);
+    }
+    for (int i = 0; i < mtrace_idx; i++) {
+      printf("%s", mtrace_ringbuf[i]);
+    }
+  }
+}
+#endif
+
 word_t vaddr_ifetch(vaddr_t addr, int len) {
   return paddr_read(addr, len);
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  word_t data = paddr_read(addr, len);
+#ifdef CONFIG_MTRACE
+  mtrace_format(mtrace_ringbuf[mtrace_idx], addr, len, data, true);
+  mtrace_idx = (mtrace_idx + 1) % MTRACE_SIZE;
+  mtrace_count++;
+#endif
+  return data;
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+  mtrace_format(mtrace_ringbuf[mtrace_idx], addr, len, data, false);
+  mtrace_idx = (mtrace_idx + 1) % MTRACE_SIZE;
+  mtrace_count++;
+#endif
   paddr_write(addr, len, data);
 }
